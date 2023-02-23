acce3501d801eaf5e842ea72f60267d527a2aab3
Mac <rocked.778@gmail.com>
2023-02-23 21:22:00 +0900

    use create-react-app

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,11 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-use handlebars::Handlebars;
-use serde::Serialize;
-
-#[derive(Serialize)]
-struct IndexTemplate {
-    title: String,
-    body: String,
-}
-
-async fn index(hb: web::Data<Handlebars<'_>>) -> impl Responder {
-    let data = IndexTemplate {
-        title: "Handlebars Example".to_string(),
-        body: "This is the body of the index page.".to_string(),
-    };
-    let body = hb.render("index", &data).unwrap();
-    HttpResponse::Ok().body(body)
-}
+use actix_files::Files;
+use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let mut handlebars = Handlebars::new();
-    handlebars
-        .register_template_file("index", "src/templates/index.hbs")
-        .unwrap();
-
-
-    HttpServer::new(move || {
+    HttpServer::new(|| {
         App::new()
-            .app_data(handlebars.clone())
-            .route("/", web::get().to(index))
+            .service(Files::new("/", "src/static/").index_file("index.html"))
     })
     .bind("127.0.0.1:8000")?
     .run()
