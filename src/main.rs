e29dce01d698a4822adff581845cfc6edaec3efb
Mac <rocked.778@gmail.com>
2023-02-23 15:18:31 +0900

    create html

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,36 @@
-use dotenv::dotenv;
-use std::env;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use handlebars::Handlebars;
+use serde::Serialize;
 
-fn main() {
-    dotenv().ok();
+#[derive(Serialize)]
+struct IndexTemplate {
+    title: String,
+    body: String,
+}
 
-    let database_url = env::var("DATABASE_URL")
-        .expect("DATABASE_URL must be set");
+async fn index(hb: web::Data<Handlebars<'_>>) -> impl Responder {
+    let data = IndexTemplate {
+        title: "Handlebars Example".to_string(),
+        body: "This is the body of the index page.".to_string(),
+    };
+    let body = hb.render("index", &data).unwrap();
+    HttpResponse::Ok().body(body)
+}
 
-    // ...
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let mut handlebars = Handlebars::new();
+    handlebars
+        .register_template_file("index", "src/templates/index.hbs")
+        .unwrap();
+
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(handlebars.clone())
+            .route("/", web::get().to(index))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
--- src/models.rs
+++ src/models.rs
@@ -1,4 +1,5 @@
 use chrono::{DateTime, Utc};
+use chrono_tz::Asia::Tokyo;
 use diesel::{Queryable, Identifiable, Associations, Insertable};
 use serde::{Deserialize, Serialize};
 
